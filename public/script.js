0ee97074165b314aa51835cf2e05a25d6a84ec88
vicwin13 <vnwimo13@gmail.com>
2022-10-15 23:42:47 +0100

    yhh

--- public/script.js
+++ public/script.js
@@ -6,3 +6,59 @@ hamburger.forEach((icon) => {
     aside.classList.toggle("show-side-nav");
   });
 });
+
+let logo = document.querySelectorAll(".logo");
+let slideLeft = document.querySelectorAll(".slideL");
+let slideRight = document.querySelectorAll(".slideR");
+let drop = document.querySelectorAll(".dropping-in");
+
+let observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio) {
+      entry.target.classList.add("flip");
+    } else {
+      entry.target.classList.remove("flip");
+    }
+  });
+});
+
+logo.forEach((logo) => {
+  observer.observe(logo);
+});
+
+let slidingLeft = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio) {
+      entry.target.classList.add("slide-left");
+    }
+  });
+});
+
+slideLeft.forEach((item) => {
+  slidingLeft.observe(item);
+});
+
+let slidingRight = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio) {
+      entry.target.classList.add("slide-right");
+    }
+  });
+});
+
+slideRight.forEach((item) => {
+  slidingRight.observe(item);
+});
+
+let droppingIn = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio) {
+      entry.target.classList.add("drop-in");
+      console.log("slide down worked");
+    }
+  });
+});
+
+drop.forEach((item) => {
+  droppingIn.observe(item);
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -42,13 +42,55 @@ module.exports = {
           0: { transform: "scaleX(0%)" },
           100: { transform: "scaleX(100%)" },
         },
+        flip: {
+          from: {
+            transform: "rotateX(-75deg)",
+            opacity: ".1",
+          },
+          to: { transform: "rotateX(0deg)", opacity: "1" },
+        },
+        slideRight: {
+          from: {
+            transform: "translateX(-30%)",
+            opacity: "0",
+          },
+          to: {
+            transform: "translateX(0%)",
+            opacity: "1",
+          },
+        },
+        slideLeft: {
+          from: {
+            transform: "translateX(30%)",
+            opacity: "0",
+          },
+          to: {
+            transform: "translateX(0%)",
+            opacity: "1",
+          },
+        },
+        dropIn: {
+          from: {
+            // transform: "translateY(-60%)",
+            opacity: "0",
+          },
+          to: {
+            //  transform: "translateY(0%)",
+            opacity: "1",
+          },
+        },
       },
+
       animation: {
         wiggle: "wiggle 7s ease-in-out infinite",
         wiggle1: " wiggle1 9s ease-in-out infinite",
         wiggle2: " wiggle2 8s ease-in-out infinite",
         rotate: "rotate 5s ease-in-out infinite",
         scaleIt: "scaleIt 10s ease-in-out ",
+        flip: "flip 1s ease-in-out forwards",
+        slideLeft: "slideLeft 1s ease-in-out forwards",
+        slideRight: "slideRight 1s ease-in-out forwards",
+        dropIn: "dropIn 4s ease-in-out 3s forwards",
       },
       backgroundImage: {
         curve1: "url('./public/images/curve_left.png')",
